9ac11dae20fc8a38c7e5f0051a4ccc074f0a5ac5
tanya-jp <t_djavaherpour@yahoo.com>
2021-07-16 01:12:26 -0700

    "Fix proc.c"

--- proc.c
+++ proc.c
@@ -91,6 +91,7 @@ allocproc(void)
 found:
   p->state = EMBRYO;
   p->pid = nextpid++;
+  p->threads = 0;
 
   release(&ptable.lock);
 
@@ -163,17 +164,28 @@ growproc(int n)
 {
   uint sz;
   struct proc *curproc = myproc();
+  struct proc *p;
 
+  acquire(&ptable.lock);
   sz = curproc->sz;
   if(n > 0){
-    if((sz = allocuvm(curproc->pgdir, sz, sz + n)) == 0)
+    if((sz = allocuvm(curproc->pgdir, sz, sz + n)) == 0){
+      release(&ptable.lock);
       return -1;
+    }
   } else if(n < 0){
-    if((sz = deallocuvm(curproc->pgdir, sz, sz + n)) == 0)
+    if((sz = deallocuvm(curproc->pgdir, sz, sz + n)) == 0){
+      release(&ptable.lock);
       return -1;
+    }
   }
   curproc->sz = sz;
+  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+    if(p->pgdir == curproc->pgdir)
+      p->sz = sz;
+  }
   switchuvm(curproc);
+  release(&ptable.lock);
   return 0;
 }
 
